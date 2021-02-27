64ea614b8a92b00fb30c244cdfcd240833b1f5e3
eren yozgat <eren190798@gmail.com>
2021-02-27 05:35:04 +0300

    24 Şubat Ders Sonu

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,5 +10,8 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string MaintenanceTİme = "Sistemimiz Bakımdadır";
         public static string ProductsListed = "Ürünler Listelendi";
+        public static string ProductCountOfCategoryError = "Bir kategorideki azami ürün sayısına ulaşıldı";
+        public static string ProductNameAlreadyExists = "Ürün ismi zaten alınmış";
+        public static string CategoryIdOver15 = "15ten fazla kategori var";
     }
 }
