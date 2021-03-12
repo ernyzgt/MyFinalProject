ec64eed862112f90110f91030f1f3d42f4ef7c52
eren yozgat <eren190798@gmail.com>
2021-03-13 01:04:29 +0300

    3 Mart Ders Sonu

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,5 +1,8 @@
 using Business.Abstract;
 using Business.Concrete;
+using Core.DependencyResolvers;
+using Core.Extentions;
+using Core.Utilities.IoC;
 using Core.Utilities.Security.Encryption;
 using Core.Utilities.Security.JWT;
 using DataAccess.Abstract;
@@ -40,6 +43,7 @@ namespace WebAPI
             //services.AddSingleton<IProductService,ProductManager>();
             //services.AddSingleton<IProductDal, EfProductDal>();
 
+
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -56,6 +60,11 @@ namespace WebAPI
                         IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
                     };
                 });
+
+            services.AddDependencyResolvers(new ICoreModule[] {
+                new CoreModule()
+            }) ;
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
